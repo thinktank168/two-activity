49c57e08150439c989ee652b5d868618847a63e6
thinktank168 <ky.tan168@gmail.com>
2017-07-11 12:50:09 +0800

    Simple 2 activity layout for testing

---
+++ app/src/main/java/itenabler/com/activityanimation/SecondActivity.java
@@ -0,0 +1,25 @@
+package itenabler.com.activityanimation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Button btn1 = (Button) findViewById(R.id.button2);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                finish();
+
+            }
+        });
+    }
+}
